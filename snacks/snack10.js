4ec50cfbf1c776df120dd434c11c08700f3ee568
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-03-30 23:13:06 +0200

    esercizio 10

--- snacks/snack10.js
+++ snacks/snack10.js
@@ -1,11 +1,11 @@
 
 
 let listNumero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
-let pari = [];
+let nonÈpari = [];
 
 for (let i = 0; i < listNumero.length; i++) {
-    if (listNumero[i] % 2 === 0) {
-        pari.push(listNumero[i]);         
+    if (listNumero[i] % 2 === 1) {
+        nonÈpari.push(listNumero[i]);         
     }
 }
-console.log(pari);
+console.log(nonÈpari);
